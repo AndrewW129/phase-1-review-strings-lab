27f32ed2040e9dbe9f7474b894964022aa2173ab
Andrew Wilson <adwilson12901@gmail.com>
2023-10-30 20:08:47 -0400

    strings

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+const currentUser = 'drew';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const initial = ['d', 'r', 'e', 'w'];
+const shortGreeting = `Welcome, ${initial[0]}!`;
